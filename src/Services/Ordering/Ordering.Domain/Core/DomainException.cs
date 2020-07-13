d43379c8eb9d7b683e0bc161251fe7e4a744a105
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-07-13 16:52:45 +0300

    Add DomainException core class.

---
+++ src/Services/Ordering/Ordering.Domain/Core/DomainException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ordering.Domain.Core
+{
+    /// <summary>
+    /// Exception type for domain exceptions
+    /// </summary>
+    public class DomainException : Exception
+    {
+        public DomainException() { }
+
+        public DomainException(string message)
+            : base(message) { }
+
+        public DomainException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
