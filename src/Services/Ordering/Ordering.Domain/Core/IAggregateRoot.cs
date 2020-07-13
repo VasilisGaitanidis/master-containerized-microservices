d31a2f712dbaf7b5631617dfa7362f83d24f68ce
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-07-13 16:51:48 +0300

    Add AggregateRoot, Repository and UnitOfWork core interfaces.

---
+++ src/Services/Ordering/Ordering.Domain/Core/IAggregateRoot.cs
@@ -0,0 +1,4 @@
+﻿namespace Ordering.Domain.Core
+{
+    public interface IAggregateRoot { }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Core/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Ordering.Domain.Core
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
