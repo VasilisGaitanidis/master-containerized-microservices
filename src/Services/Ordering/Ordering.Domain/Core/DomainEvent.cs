0d19c81b8c1565680b0d687c2c7e6d191d8ba3f5
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-07-13 15:30:08 +0300

    Add Ordering.Domain project.
    Add Core Entity and DomainEvent classes.

---
+++ src/Services/Ordering/Ordering.Domain/Core/DomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Core
+{
+    public abstract class DomainEvent : INotification
+    {
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Core/Entity.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ordering.Domain.Core
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; protected set; }
+
+        private List<DomainEvent> _domainEvents;
+        public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents?.AsReadOnly();
+
+        public void AddDomainEvent(DomainEvent domainEvent)
+        {
+            _domainEvents ??= new List<DomainEvent>();
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void RemoveDomainEvent(DomainEvent domainEvent)
+        {
+            _domainEvents?.Remove(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents?.Clear();
+        }
+
+        #region BaseBehaviours
+
+        public bool Equals(Entity other)
+        {
+            if (ReferenceEquals(this, other)) return true;
+            return !ReferenceEquals(null, other) && Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Entity);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (a is null && b is null)
+                return true;
+
+            if (a is null || b is null)
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() ^ 93) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+
+        #endregion
+    }
+}
