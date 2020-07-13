728d358cf5d78e23817f3547dc5245fa61e70279
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-07-13 23:57:47 +0300

    Refactor core domain AggregateRoot, Entity and Identity classes

---
+++ src/BuildingBlocks/Domain.Core/AggregateRoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Domain.Core.Messaging;
+
+namespace Domain.Core
+{
+    public interface IAggregateRoot<out TId> : IEntity<TId>
+    {
+        void AddDomainEvent(DomainEvent domainEvent);
+
+        void RemoveDomainEvent(DomainEvent domainEvent);
+
+        void ClearDomainEvents();
+    }
+
+    public abstract class AggregateRoot<TId> : Entity<TId>, IAggregateRoot<TId>
+    {
+        private List<DomainEvent> _domainEvents;
+        public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents?.AsReadOnly();
+
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
+
+        public void AddDomainEvent(DomainEvent domainEvent)
+        {
+            _domainEvents ??= new List<DomainEvent>();
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void RemoveDomainEvent(DomainEvent domainEvent)
+            => _domainEvents?.Remove(domainEvent);
+
+        public void ClearDomainEvents()
+            => _domainEvents?.Clear();
+    }
+}
---
+++ src/BuildingBlocks/Domain.Core/Entity.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Core
+{
+    public interface IEntity<out TId> : IIdentity<TId>
+    {
+    }
+
+    public abstract class Entity<TId> : IEntity<TId>
+    {
+        protected Entity(TId id)
+        {
+            Id = id;
+        }
+
+        /// <inheritdoc />
+        public TId Id { get; }
+    }
+}
