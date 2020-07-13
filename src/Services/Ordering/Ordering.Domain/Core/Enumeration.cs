c3a37f3b4f69594f295907d0b23d88f066b55376
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-07-13 16:50:41 +0300

    Add Core ValueObject and Enumeration abstract classes.

---
+++ src/Services/Ordering/Ordering.Domain/Core/Enumeration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ordering.Domain.Core
+{
+    /// <summary>
+    /// Enumeration class based on https://lostechies.com/jimmybogard/2008/08/12/enumeration-classes/
+    /// </summary>
+    public abstract class Enumeration : IComparable
+    {
+        public int Id { get; }
+
+        public string Name { get; }
+
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Enumeration otherValue))
+                return false;
+
+            var typeMatches = GetType() == obj.GetType();
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public override string ToString() => Name;
+
+        public int CompareTo(object obj)
+            => Id.CompareTo(((Enumeration)obj).Id);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Core/ValueObject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ordering.Domain.Core
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public static bool operator ==(ValueObject obj1, ValueObject obj2)
+        {
+            if (ReferenceEquals(obj1, null) ^ ReferenceEquals(obj2, null))
+            {
+                return false;
+            }
+            return ReferenceEquals(obj1, null) || obj1.Equals(obj2);
+        }
+
+        public static bool operator !=(ValueObject obj1, ValueObject obj2)
+        {
+            return !(obj1 == obj2);
+        }
+
+        public bool Equals(ValueObject other)
+        {
+            return Equals(other as object);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+            var other = (ValueObject)obj;
+            using IEnumerator<object> thisValues = GetEqualityComponents().GetEnumerator();
+            using IEnumerator<object> otherValues = other.GetEqualityComponents().GetEnumerator();
+            while (thisValues.MoveNext() && otherValues.MoveNext())
+            {
+                if (ReferenceEquals(thisValues.Current, null) ^ ReferenceEquals(otherValues.Current, null))
+                {
+                    return false;
+                }
+                if (thisValues.Current != null && !thisValues.Current.Equals(otherValues.Current))
+                {
+                    return false;
+                }
+            }
+            return !thisValues.MoveNext() && !otherValues.MoveNext();
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Select(x => x != null ? x.GetHashCode() : 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+    }
+}
