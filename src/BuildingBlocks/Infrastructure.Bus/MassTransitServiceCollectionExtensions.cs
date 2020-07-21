9154448bec4a72f6c921471d2415f74e56a81d4e
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-07-21 14:04:45 +0300

    Demo MassTransit implementation

--- src/BuildingBlocks/Infrastructure.Bus/MassTransitServiceCollectionExtensions.cs
+++ src/BuildingBlocks/Infrastructure.Bus/MassTransitServiceCollectionExtensions.cs
@@ -1,45 +1,103 @@
-﻿using System;
+﻿using System.Threading.Tasks;
 using Common;
 using Infrastructure.Bus.Middlewares;
 using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Bus
 {
     public static class MassTransitServiceCollectionExtensions
     {
-        public static IServiceCollection AddMassTransit1(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddProducer(this IServiceCollection services, IConfiguration configuration)
         {
+            var rabbitMqOptions = configuration.GetOptions<RabbitMqOptions>("rabbitMq");
 
-            throw new NotImplementedException();
-            //services.AddMassTransit(configurator =>
-            //{
-            //    var rabbitMqOptions = configuration.GetOptions<RabbitMqOptions>("rabbitMq");
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq((context, configurator) =>
+                {
+                    configurator.Host(rabbitMqOptions.Host, rabbitMqOptions.VirtualHost, hostConfigurator =>
+                    {
+                        hostConfigurator.Username(rabbitMqOptions.Username);
+                        hostConfigurator.Password(rabbitMqOptions.Password);
+                    });
+                });
+            });
 
-            //    return Bus.Factory.CreateUsingRabbitMq(cfg =>
-            //    {
-            //        var host = cfg.Host(new Uri(rabbitMqOptions.Url), "/", hc =>
-            //        {
-            //            hc.Username(rabbitMqOptions.UserName);
-            //            hc.Password(rabbitMqOptions.Password);
-            //        });
+            services.AddMassTransitHostedService();
 
-            //        cfg.ReceiveEndpoint("contact", x =>
-            //        {
-            //            x.ConfigureConsumer<ContactCreatedConsumer>(provider);
-            //        });
+            return services;
+        }
 
-            //        cfg.PropagateOpenTracingContext();
-            //        cfg.PropagateCorrelationIdContext();
-            //    });
-            //}, (cfg) =>
-            //{
-            //    cfg.AddConsumersFromNamespaceContaining<ConsumerAnchor>();
-            //});
+        public static IServiceCollection AddConsumer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var rabbitMqOptions = configuration.GetOptions<RabbitMqOptions>("rabbitMq");
 
-            //return services;
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<EventConsumer>();
 
+                x.UsingRabbitMq((context, configurator) =>
+                {
+                    // configurator.SendTopology.UseCorrelationId<IMessage>(message => message.CorrelationId);
+
+                    configurator.Host(rabbitMqOptions.Host, rabbitMqOptions.VirtualHost, hostConfigurator =>
+                    {
+                        hostConfigurator.Username(rabbitMqOptions.Username);
+                        hostConfigurator.Password(rabbitMqOptions.Password);
+                    });
+
+                    configurator.ReceiveEndpoint("event-listener", e =>
+                    {
+                        e.ConfigureConsumer<EventConsumer>(context);
+                    });
+                });
+            });
+
+            services.AddMassTransitHostedService();
+
+            return services;
+        }
+
+        public static IServiceCollection AddRabbitMq(this IServiceCollection services, IConfiguration configuration)
+        {
+            var rabbitMqOptions = configuration.GetOptions<RabbitMqOptions>("rabbitMq");
+
+            services.AddMassTransit(configurator =>
+            {
+                MassTransit.Bus.Factory.CreateUsingRabbitMq(factoryConfigurator =>
+                {
+                    factoryConfigurator.Host(rabbitMqOptions.Host, rabbitMqOptions.VirtualHost, hostConfigurator =>
+                    {
+                        hostConfigurator.Username(rabbitMqOptions.Username);
+                        hostConfigurator.Password(rabbitMqOptions.Password);
+                    });
+                });
+            });
+
+            return services;
+        }
+    }
+
+    internal interface IValueEntered
+    {
+        string Value { get; }
+    }
+
+    class EventConsumer : IConsumer<IValueEntered>
+    {
+        readonly ILogger<EventConsumer> _logger;
+
+        public EventConsumer(ILogger<EventConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<IValueEntered> context)
+        {
+            _logger.LogInformation("Value: {Value}", context.Message.Value);
         }
     }
 }
---
+++ src/BuildingBlocks/Infrastructure.Bus/Middlewares/IMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Infrastructure.Bus.Middlewares
+{
+    public interface IMessage
+    {
+        Guid Id { get; }
+
+        Guid CorrelationId { get; }
+
+        DateTime? CreatedDate { get; }
+    }
+}
