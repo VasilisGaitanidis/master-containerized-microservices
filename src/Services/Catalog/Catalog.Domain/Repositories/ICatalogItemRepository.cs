e10fd5392c5ca442d1a9a7e226abbcb2d3aea162
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-08-06 19:47:25 +0300

    Add domain catalog item repository

---
+++ src/Services/Catalog/Catalog.Domain/Repositories/ICatalogItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Catalog.Domain.Models;
+using Domain.Core.Data;
+
+namespace Catalog.Domain.Repositories
+{
+    public interface ICatalogItemRepository : IRepository<CatalogItem, Guid>
+    {
+        Task<CatalogItem> GetCatalogItemAsync(Guid id);
+
+        Task<CatalogItem> AddAsync(CatalogItem catalogItem);
+
+        Task UpdateAsync(CatalogItem catalogItem);
+
+        Task DeleteAsync(Guid id);
+    }
+}
