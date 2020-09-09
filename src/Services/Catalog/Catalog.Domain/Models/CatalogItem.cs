6eb36646563bf1fd42074c7b7ac1862044ca336c
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-09-09 21:38:09 +0300

    Add private constructor on catalog item because of EF Core cannot set navigation properties using a constructor.

--- src/Services/Catalog/Catalog.Domain/Models/CatalogItem.cs
+++ src/Services/Catalog/Catalog.Domain/Models/CatalogItem.cs
@@ -16,8 +16,33 @@ namespace Catalog.Domain.Models
         public CatalogType CatalogType { get; private set; }
         private Guid _catalogTypeId;
 
+        /// <summary>
+        /// Private constructor without navigational properties (eg. CatalogType)
+        /// </summary>
+        /// <param name="id">The catalog item identifier.</param>
+        /// <param name="name">The catalog item name.</param>
+        /// <param name="description">The catalog item description.</param>
+        /// <param name="price">The catalog item price.</param>
+        /// <param name="stock">The catalog item stock.</param>
+        private CatalogItem(Guid id, string name, string description, decimal price, int stock)
+            : base(id)
+        {
+            _name = name;
+            _description = description;
+            _price = price;
+            _stock = stock;
+        }
+
+        /// <summary>
+        /// Public constructor with navigational properties.
+        /// </summary>
+        /// <param name="name">The catalog item name.</param>
+        /// <param name="description">The catalog item description.</param>
+        /// <param name="price">The catalog item price.</param>
+        /// <param name="stock">The catalog item stock.</param>
+        /// <param name="catalogType">The catalog item type.</param>
         public CatalogItem(string name, string description, decimal price, int stock, CatalogType catalogType)
-            : base(Guid.NewGuid())
+            : this(Guid.NewGuid(), name, description, price, stock)
         {
             _name = name;
             _description = description;
