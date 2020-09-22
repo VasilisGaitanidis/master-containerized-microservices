2234e8203d07622b627d11e7cf5c246765b1494d
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-09-22 23:45:24 +0300

    Catalog svc. Add UpdateSchema migration
    dotnet ef migrations add UpdateSchema -s .\Catalog.Api\ --output-dir Infrastructure\Migrations
    dotnet ef database update -s .\Catalog.Api\

--- src/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20200922204239_UpdateSchema.Designer.cs
+++ src/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20200922204239_UpdateSchema.Designer.cs
@@ -4,14 +4,16 @@ using Catalog.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Catalog.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(CatalogDataContext))]
-    partial class CatalogDataContextModelSnapshot : ModelSnapshot
+    [Migration("20200922204239_UpdateSchema")]
+    partial class UpdateSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
