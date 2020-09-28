7ed35e8f02a3c69cb970eb8fbdaac1517af38956
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-09-28 20:28:56 +0300

    Catalog svc. Removed unused property from UpdateCatalogItemDto.

--- src/Services/Catalog/Catalog.Api/Application/Dtos/Requests/UpdateCatalogItemDto.cs
+++ src/Services/Catalog/Catalog.Api/Application/Dtos/Requests/UpdateCatalogItemDto.cs
@@ -4,8 +4,6 @@ namespace Catalog.Api.Application.Dtos.Requests
 {
     public class UpdateCatalogItemDto
     {
-        public Guid Id { get; set; }
-
         public string Name { get; set; }
 
         public string Description { get; set; }
