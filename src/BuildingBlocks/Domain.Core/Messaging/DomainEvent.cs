b72b0d3f62b088f60310fdd94bd3c5935a2211f1
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-10-04 22:38:28 +0300

    BuildingBlocks. Add Event / IntegrationEvent / DomainEvent.

--- src/BuildingBlocks/Domain.Core/Messaging/DomainEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace Domain.Core.Messaging
-{
-    public abstract class DomainEvent : INotification
-    {
-    }
-}
---
+++ src/BuildingBlocks/Domain.Core/Messaging/Event.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Domain.Core.Messaging
+{
+    /// <summary>
+    /// The event interface.
+    /// </summary>
+    public interface IEvent
+    {
+        /// <summary>
+        /// The event identifier.
+        /// </summary>
+        Guid Id { get; }
+
+        /// <summary>
+        /// The event/aggregate root version
+        /// </summary>
+        int EventVersion { get; }
+
+        /// <summary>
+        /// The date the <see cref="Event"/> occurred on.
+        /// </summary>
+        DateTime OccurredOn { get; }
+    }
+
+    /// <summary>
+    /// The integration event interface.
+    /// </summary>
+    public interface IIntegrationEvent : IEvent
+    {
+    }
+
+    /// <summary>
+    /// The domain event interface.
+    /// </summary>
+    public interface IDomainEvent : IEvent
+    {
+    }
+
+    /// <inheritdoc cref="IEvent"/>
+    public abstract class Event : IEvent
+    {
+        public Guid Id { get; protected set; } = Guid.NewGuid();
+
+        public int EventVersion { get; protected set; } = 1;
+
+        public DateTime OccurredOn { get; protected set; } = DateTime.UtcNow;
+    }
+
+    public abstract class IntegrationEvent : Event, IIntegrationEvent
+    {
+    }
+
+    public abstract class DomainEvent : Event, IDomainEvent
+    {
+    }
+}
