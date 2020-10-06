1c842d7592fd8c10cc89a38b32fc5097632290a8
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-10-06 23:21:16 +0300

    Infrastructure. Add Messaging data context with outbox message entity and configuration

---
+++ src/BuildingBlocks/Infrastructure/Messaging/OutboxMessageEntityTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Messaging
+{
+    public class OutboxMessageEntityTypeConfiguration : IEntityTypeConfiguration<OutboxMessage>
+    {
+        public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+        {
+            builder.ToTable("OutboxMessages", MessagingDataContext.DefaultSchema);
+
+            builder.HasKey(x => x.Id);
+
+            builder.Ignore(x => x.DomainEvents);
+
+            builder.Property(x => x.OccurredOn)
+                .IsRequired();
+
+            builder.Property(x => x.Type)
+                .IsRequired();
+
+            builder.Property(x => x.Data)
+                .IsRequired();
+
+            builder.Property(x => x.ProcessedOn)
+                .IsRequired(false);
+        }
+    }
+}
