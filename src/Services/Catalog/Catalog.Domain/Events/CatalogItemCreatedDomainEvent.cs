f62f29a5ada16968cd7a4fd83dda6cde04709de4
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-10-18 18:17:01 +0300

    Catalog svc. Add Catalog domain events.

---
+++ src/Services/Catalog/Catalog.Domain/Events/CatalogItemCreatedDomainEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Domain.Core.Messaging;
+
+namespace Catalog.Domain.Events
+{
+    public class CatalogItemCreatedDomainEvent : DomainEvent
+    {
+        public string Name { get; }
+
+        public string Description { get; }
+
+        public decimal Price { get; }
+
+        public int Stock { get; }
+
+        public Guid CatalogTypeId { get; }
+
+        public CatalogItemCreatedDomainEvent(string name, string description, decimal price, int stock, Guid catalogTypeId)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            CatalogTypeId = catalogTypeId;
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Domain/Events/CatalogItemPriceChangedDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Core.Messaging;
+
+namespace Catalog.Domain.Events
+{
+    public class CatalogItemPriceChangedDomainEvent : DomainEvent
+    {
+        public decimal Price { get; }
+
+        public CatalogItemPriceChangedDomainEvent(decimal price)
+        {
+            Price = price;
+        }
+    }
+}
