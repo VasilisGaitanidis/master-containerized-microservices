fd0c78495773afbadf74c118a24dd8118c4115e8
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-10-28 13:35:04 +0200

    Catalog svc. Add Exception filter.

---
+++ src/Services/Catalog/Catalog.Api/Filters/HttpGlobalExceptionFilter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Domain.Core.Exceptions;
+using FluentValidation;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.Api.Filters
+{
+    public class HttpGlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly ILogger<HttpGlobalExceptionFilter> _logger;
+
+        public HttpGlobalExceptionFilter(IWebHostEnvironment hostingEnvironment, ILogger<HttpGlobalExceptionFilter> logger)
+        {
+            _hostingEnvironment = hostingEnvironment ?? throw new ArgumentNullException(nameof(hostingEnvironment));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(new EventId(context.Exception.HResult),
+                context.Exception,
+                context.Exception.Message);
+
+            if (_hostingEnvironment.IsDevelopment())
+            {
+                return;
+            }
+
+            if (context.Exception is DomainException domainException)
+            {
+                var problemDetails = new ValidationProblemDetails
+                {
+                    Instance = context.HttpContext.Request.Path,
+                    Status = StatusCodes.Status400BadRequest,
+                    Detail = "Please refer to the errors property for additional details."
+                };
+
+                problemDetails.Errors.Add("DomainValidationErrors", new[] { context.Exception.Message });
+
+                if (domainException.InnerException is ValidationException validationException)
+                {
+                    problemDetails.Errors.Add("CommandValidationErrors", validationException.Errors.Select(x => x.ErrorMessage).ToArray());
+                }
+
+                context.Result = new BadRequestObjectResult(problemDetails);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
