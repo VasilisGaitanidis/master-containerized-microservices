d07dfb96d101e8226c12260fe3e3a8811ed11938
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-11-08 21:47:14 +0200

    [Contracts] Added shared project Contracts for integration events.

--- src/Services/Catalog/Catalog.Application/UseCases/Events/CatalogItemCreated/CatalogItemCreatedDomainEventHandler.cs
+++ src/Services/Catalog/Catalog.Application/UseCases/Events/CatalogItemCreated/CatalogItemCreatedDomainEventHandler.cs
@@ -2,15 +2,31 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Catalog.Domain.Events;
+using Contracts.IntegrationEvents;
+using MassTransit;
 using MediatR;
 
 namespace Catalog.Application.UseCases.Events.CatalogItemCreated
 {
     public class CatalogItemCreatedDomainEventHandler : INotificationHandler<CatalogItemCreatedDomainEvent>
     {
-        public Task Handle(CatalogItemCreatedDomainEvent notification, CancellationToken cancellationToken)
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public CatalogItemCreatedDomainEventHandler(IPublishEndpoint publishEndpoint)
         {
-            throw new NotImplementedException();
+            _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+        }
+
+        public async Task Handle(CatalogItemCreatedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            await _publishEndpoint.Publish<CatalogItemCreatedIntegrationEvent>(new
+            {
+                notification.Name,
+                notification.Description,
+                notification.Price,
+                notification.Stock,
+                notification.CatalogTypeId
+            }, cancellationToken);
         }
     }
 }
