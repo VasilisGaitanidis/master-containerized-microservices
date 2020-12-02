8387556cda42079a3418cdab22d848228edaea1b
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-12-02 21:41:27 +0200

    [ApiGateway] Added Swagger for Ocelot API Gateway.

--- src/ApiGateways/Gateway.WebApi/Startup.cs
+++ src/ApiGateways/Gateway.WebApi/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
@@ -10,11 +11,19 @@ namespace Gateway.WebApi
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services
+                .AddSwaggerForOcelot(Configuration)
                 .AddOcelot()
                 .AddConsul();
         }
@@ -26,6 +35,8 @@ namespace Gateway.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwaggerForOcelotUI();
 
             app.UseRouting();
 
