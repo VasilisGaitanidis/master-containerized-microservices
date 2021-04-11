ffc2c2df3c4b32cbb7d751d26d267d0237129396
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2021-04-11 21:32:08 +0300

    [Discount svc] Updated Migration script.

--- src/Services/Discount/Discount.Infrastructure/Scripts/Migration.sql
+++ src/Services/Discount/Discount.Infrastructure/Scripts/Migration.sql
@@ -28,3 +28,22 @@ VALUES (N'20210411121516_InitialMigration', N'3.1.9');
 
 GO
 
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[discount].[Coupons]') AND [c].[name] = N'ProductName');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [discount].[Coupons] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [discount].[Coupons] ALTER COLUMN [ProductName] nvarchar(450) NULL;
+
+GO
+
+CREATE UNIQUE INDEX [IX_Coupons_ProductName] ON [discount].[Coupons] ([ProductName]) WHERE [ProductName] IS NOT NULL;
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210411182728_UpdateCouponProductName', N'3.1.9');
+
+GO
+
