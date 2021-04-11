261cdf229e68c6fdb4cc7474c80a0b15cfe83df7
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2021-04-11 20:04:08 +0300

    [Discount svc] Added Logging and Validator Pipeline Behaviors on ServiceCollectionExtensions.

---
+++ src/Services/Discount/Discount.Application/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Discount.Application.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+
+            var response = await next();
+
+            _logger.LogInformation($"Handled {typeof(TRequest).Name}");
+
+            return response;
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using AutoMapper;
+using Discount.Application.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Discount.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDiscountApplication(this IServiceCollection services)
+        {
+            services
+                .AddCqrsHandlers()
+                .AddPipelineBehaviors()
+                .AddValidators()
+                .AddMappings();
+
+            return services;
+        }
+
+        public static IServiceCollection AddCqrsHandlers(this IServiceCollection services)
+        {
+            return services
+                .AddMediatR(Assembly.GetExecutingAssembly());
+        }
+
+        public static IServiceCollection AddPipelineBehaviors(this IServiceCollection services)
+        {
+            return services
+                .AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>))
+                .AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
+        }
+
+        public static IServiceCollection AddValidators(this IServiceCollection services)
+        {
+            return services
+                .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public static IServiceCollection AddMappings(this IServiceCollection services)
+        {
+            return services
+                .AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
