424e9c03871d497b29fe73715f1ab0f01c45493f
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2021-04-11 21:48:37 +0300

    [Discount svc] Added CouponNotFoundException on application layer.

---
+++ src/Services/Discount/Discount.Application/Exceptions/CouponNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Application.Exceptions;
+
+namespace Discount.Application.Exceptions
+{
+    public class CouponNotFoundException : EntityNotFoundException
+    {
+        public CouponNotFoundException(string productName)
+            : base($"Coupon with product name '{productName}' not found.")
+        {
+            ProductName = productName;
+        }
+
+        public string ProductName { get; }
+    }
+}
