93a3173ecc38d9683cacc74b6406ddb80408f2cd
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2021-04-11 21:53:17 +0300

    [Discount svc] Added AutoMapper coupon profile.

---
+++ src/Services/Discount/Discount.Application/Helpers/CouponProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Discount.Application.Dtos.Responses;
+using Discount.Domain.Entities;
+
+namespace Discount.Application.Helpers
+{
+    public class CouponProfiles : Profile
+    {
+        public CouponProfiles()
+        {
+            CreateMap<Coupon, CouponDto>();
+        }
+    }
+}
