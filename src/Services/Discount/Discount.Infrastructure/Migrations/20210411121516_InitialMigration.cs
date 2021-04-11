9e62330338849a19a918991c11f686bee5017c59
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2021-04-11 15:19:31 +0300

    [Discount svc] Added entity framework initial migration.

---
+++ src/Services/Discount/Discount.Infrastructure/Migrations/20210411121516_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Discount.Infrastructure.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "discount");
+
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                schema: "discount",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Amount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons",
+                schema: "discount");
+        }
+    }
+}
