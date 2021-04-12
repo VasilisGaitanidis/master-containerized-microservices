d778dfd40a9be607effe88e2ce21606614322b9d
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2021-04-12 18:43:53 +0300

    [Discount svc] Implement create coupon command.

--- src/Services/Discount/Discount.Application/UseCases/Commands/CreateCoupon/CreateCouponCommandValidator.cs
+++ src/Services/Discount/Discount.Application/UseCases/Commands/CreateCoupon/CreateCouponCommandValidator.cs
@@ -2,8 +2,14 @@
 
 namespace Discount.Application.UseCases.Commands.CreateCoupon
 {
+    /// <summary>
+    /// The create coupon command validator.
+    /// </summary>
     public class CreateCouponCommandValidator : AbstractValidator<CreateCouponCommand>
     {
+        /// <summary>
+        /// Initializes a new instance of a <see cref="CreateCouponCommandValidator"/>.
+        /// </summary>
         public CreateCouponCommandValidator()
         {
             RuleFor(c => c.ProductName)
