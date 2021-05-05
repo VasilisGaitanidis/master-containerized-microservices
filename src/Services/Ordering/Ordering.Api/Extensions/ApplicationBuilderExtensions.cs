9e9c70dad12e74c3e78fb162006ca315498278d1
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2021-05-05 17:33:51 +0300

    [Ordering svc] Added initial Ordering Api and Application layers.

---
+++ src/Services/Ordering/Ordering.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Ordering.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseCustomSwagger(this IApplicationBuilder app)
+        {
+            return app.UseSwagger().UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API V1");
+            });
+        }
+    }
+}
