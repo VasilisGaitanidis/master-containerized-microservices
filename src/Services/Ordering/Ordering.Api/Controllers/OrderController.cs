e1ddcedacf28c47a998847e91e64683b745965c9
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2021-05-09 13:36:39 +0300

    [Ordering svc] Implemented Get Orders Query.

---
+++ src/Services/Ordering/Ordering.Api/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Dtos.Responses;
+using Ordering.Application.UseCases.Queries.GetOrders;
+
+namespace Ordering.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet("[action]")]
+        [ProducesResponseType(typeof(IEnumerable<OrderDto>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrdersAsync()
+            => Ok(await _mediator.Send(new GetOrdersQuery()));
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/UseCases/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using Ordering.Application.Dtos.Responses;
+
+namespace Ordering.Application.UseCases.Queries.GetOrders
+{
+    public class GetOrdersQuery : IRequest<IEnumerable<OrderDto>>
+    {
+    }
+}
